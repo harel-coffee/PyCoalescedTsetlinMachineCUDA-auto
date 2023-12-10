65e47139534eb90082fcd904fe58037882d31bb5
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-12-10 01:49:32 +0100

    Update

--- examples/IMDbConvolutionEmbeddingTextCategorizationDemo.py
+++ examples/IMDbConvolutionEmbeddingTextCategorizationDemo.py
@@ -53,7 +53,7 @@ print("Retrieving embeddings...")
 encoding = {}
 #f = open("/data/near-lossless-binarization/binary_vectors_1024.vec", "r")
 #f = open("/data/near-lossless-binarization/binary_vectors_fasttext_256.vec", "r")
-f = open("/data/near-lossless-binarization/binary_vectors_bayesian_1024_5000_100.bin", "r")
+f = open("/data/near-lossless-binarization/wiki_binary_vectors_bayesian_1024_5000_50.bin", "r")
 
 line = f.readline()
 line = f.readline().strip()
